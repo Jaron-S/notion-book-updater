9da1c57f7af7b94bc71b7a8c2195751296795d4b
Jaron-S <jaronschoor@gmail.com>
2024-04-10 16:06:16 -0600

    added cron

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require("express");
+const app = express();
+
+const cron = require("node-cron");
+const { fetchAndUpdate } = require("./modules");
+
+// Schedule tasks to be run on the server every 15 minutes.
+cron.schedule("*/15 * * * *", function () {
+  console.log("Running fetchAndUpdate every 15 minutes");
+  fetchAndUpdate();
+});
+
+app.all("/", (req, res) => {
+  console.log("Just got a request!");
+  res.send("Updating books");
+  fetchAndUpdate();
+});
+
+app.listen(process.env.PORT || 3000);
